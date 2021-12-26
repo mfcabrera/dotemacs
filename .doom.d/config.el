4af01eeb66a143c48b4077222c8dba3fd53272b4
Miguel Cabrera <miguel.cabrera@shopify.com>
2021-12-26 11:04:12 +0100

    add org-roam-ui config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -617,10 +617,17 @@
   :hook (after-init . shadowenv-global-mode)
 )
 
+(use-package! websocket
+  :after org-roam)
 
-(use-package! vulpea
-;;  :ensure t
-;; hook int org-roam-db-autosync-mode you wish to enable
-;; persistence of meta values (see respective section in README to
-;; find out what meta means)
-)
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
